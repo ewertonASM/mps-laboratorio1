8a98973546c2ad779ccf69398701b57205a218ac
ewertonASM <ewerton-andre@outlook.com>
2021-10-25 12:15:41 -0300

    fix deploy

---
+++ src/main/java/br/mps/App.java
@@ -0,0 +1,14 @@
+package br.mps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
